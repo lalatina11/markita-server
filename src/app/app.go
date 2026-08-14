eb82f74a14ec777a25e09afd18f0bac6ce02da7c
lalatina11 <rahmadancandra@gmail.com>
2026-08-14 08:06:29 +0700

    feat: integrate route registration into application initialization

--- src/app/app.go
+++ src/app/app.go
@@ -1,9 +1,14 @@
 package app
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"github.com/gofiber/fiber/v3"
+	"github.com/lalatina11/markita.git/src/routes"
+)
 
 func App() *fiber.App {
 	app := fiber.New()
+
+	routes.AppRoutes(app)
 
 	return app
 }
