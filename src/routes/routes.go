a307f3ecfee54b582f5f656b781723b5fe081a70
lalatina11 <rahmadancandra@gmail.com>
2026-08-15 16:09:18 +0700

    feat: add request logging middleware to fiber application routes

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -2,10 +2,15 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/logger"
 	"github.com/lalatina11/markita.git/src/handler"
 )
 
 func AppRoutes(app *fiber.App) {
+	app.Use(logger.New(logger.Config{
+		Format: "# ${method} ${path} - ${status}\n",
+	}))
+
 	appHandler := handler.NewAppHandler()
 	app.Get("/", appHandler.Root)
 }
