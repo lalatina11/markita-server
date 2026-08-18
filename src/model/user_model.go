0c00ddb398e986f613ee15beb5b28deae2648d76
lalatina11 <rahmadancandra@gmail.com>
2026-08-18 17:24:30 +0700

    feat: add json tags to CreatedAt, UpdatedAt, and DeletedAt fields in User model

--- src/model/user_model.go
+++ src/model/user_model.go
@@ -7,11 +7,11 @@ import (
 )
 
 type User struct {
-	ID          string `json:"id" gorm:"primaryKey,index"`
-	DisplayName string `json:"display_name" gorm:"index"`
-	Email       string `json:"email" gorm:"unique,index"`
-	Avatar      string `json:"avatar"`
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   gorm.DeletedAt `gorm:"index"`
+	ID          string         `json:"id" gorm:"primaryKey,index"`
+	DisplayName string         `json:"display_name" gorm:"index"`
+	Email       string         `json:"email" gorm:"unique,index"`
+	Avatar      string         `json:"avatar"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 }
