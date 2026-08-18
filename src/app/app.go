38a8c69fa1a40ad5c313f669403665f65ecb0347
lalatina11 <rahmadancandra@gmail.com>
2026-08-18 16:41:54 +0700

    feat: initialize database connection and auto-migrate User model in application startup

--- src/app/app.go
+++ src/app/app.go
@@ -2,10 +2,14 @@ package app
 
 import (
 	"github.com/gofiber/fiber/v3"
+	"github.com/lalatina11/markita.git/src/config"
+	"github.com/lalatina11/markita.git/src/model"
 	"github.com/lalatina11/markita.git/src/routes"
 )
 
 func App() *fiber.App {
+	db := config.NewDatabaseConfig().Connect()
+	db.AutoMigrate(&model.User{})
 	app := fiber.New()
 
 	routes.AppRoutes(app)
